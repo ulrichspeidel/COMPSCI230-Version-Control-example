4725dcf01355455039719361dded6643d2dc9b8a
ulrichspeidel <ulrich@cs.auckland.ac.nz>
2016-04-12 14:40:47 +1200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -14,7 +14,11 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		System.out.println("Hello World - I'm edited!");
+	}
+	
+	private void doOtherStuff() {
+		System.out.println("I do other stuff!");
 	}
 
 }
