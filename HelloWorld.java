456f5e15c14770812eef986c708dc9a95b00ecca
ulrichspeidel <ulrich@cs.auckland.ac.nz>
2016-04-14 12:44:46 +1200

    Update HelloWorld.java
    Changed the greeting so we can see that it's from me, Bob! I also added my own method for future use.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World! This is Bob!");
+	}
+	
+	public static void bobsMethod() {
+		System.out.println("This is Bob's method!");
 	}
 }
